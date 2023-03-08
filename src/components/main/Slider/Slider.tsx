c4b2528c2ee1bb1054c669187eb8e1053b172865
dmtr636 <dmtr636@gmail.com>
2023-03-08 16:48:20 +0300

    Refactoring

--- src/components/main/Slider/Slider.tsx
+++ src/components/main/Slider/Slider.tsx
@@ -7,73 +7,74 @@ import "swiper/css/pagination";
 
 import styles from "./style.module.scss";
 import Button from "@/components/ui/Button/Button";
-import { PaginationOptions } from "swiper/types";
 import Image from "next/image";
 import classNames from "classnames";
+import { ISlide } from "@/interfaces/ISlide";
 
-const slides = [
+const slides: ISlide[] = [
     {
         header: "Местоположение заголовка названия слайдера",
         text: "Текст, который будет находится в контейнере слайдера",
         buttonText: "Кнопка действия",
         bgUrl: "https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
-        bgColor: "light",
+        textColor: "dark",
     },
     {
         header: "Эксперт месяца",
         text: "Текст, который будет находится в контейнере слайдера",
         buttonText: "Кнопка действия",
         bgUrl: "https://images.unsplash.com/photo-1554310212-e1d9f0d4d9be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        bgColor: "light",
+        textColor: "dark",
     },
     {
         header: "Заголовок очень большой прям зачем такой делать не\xA0знаю, но мало ли",
         text: "Текст, который будет находится в контейнере слайдера",
         buttonText: "Кнопка действия",
         bgUrl: "https://images.unsplash.com/photo-1531704118376-ec637130bfa0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        bgColor: "dark",
+        textColor: "light",
     },
 ];
 
 const Slider = () => {
-    const pagination: PaginationOptions = {
-        clickable: true,
-        horizontalClass: styles.pagination,
-        bulletClass: styles.bullet,
-        bulletActiveClass: styles.active,
-    };
+    const renderSlide = (slide: ISlide, index: number) => (
+        <SwiperSlide
+            key={slide.header}
+            className={classNames(styles.slide, {
+                [styles.textLight]: slide.textColor === "light",
+            })}
+        >
+            {slide.bgUrl && (
+                <Image
+                    src={slide.bgUrl}
+                    alt={""}
+                    fill={true}
+                    style={{ zIndex: "-1", objectFit: "cover" }}
+                    priority={index === 0}
+                />
+            )}
+            <h1 className={styles.header}>{slide.header}</h1>
+            <div className={styles.text}>{slide.text}</div>
+            <div className={styles.button}>
+                <Button>{slide.buttonText}</Button>
+            </div>
+        </SwiperSlide>
+    );
 
     return (
         <div className={styles.sliderWrapper}>
             <Swiper
-                pagination={pagination}
                 modules={[Pagination, Autoplay]}
+                pagination={{
+                    clickable: true,
+                    horizontalClass: styles.pagination,
+                    bulletClass: styles.bullet,
+                    bulletActiveClass: styles.active,
+                }}
                 className={styles.slider}
                 loop={true}
                 autoplay={{ delay: 4000 }}
             >
-                {slides.map((slide) => (
-                    <SwiperSlide
-                        key={slide.header}
-                        className={classNames(styles.slide, {
-                            [styles.bgDark]: slide.bgColor === "dark",
-                        })}
-                    >
-                        {slide.bgUrl && (
-                            <Image
-                                src={slide.bgUrl}
-                                alt={""}
-                                fill={true}
-                                style={{ zIndex: "-1", objectFit: "cover" }}
-                            />
-                        )}
-                        <h1 className={styles.header}>{slide.header}</h1>
-                        <div className={styles.text}>{slide.text}</div>
-                        <div className={styles.button}>
-                            <Button>{slide.buttonText}</Button>
-                        </div>
-                    </SwiperSlide>
-                ))}
+                {slides.map(renderSlide)}
             </Swiper>
         </div>
     );
---
+++ src/interfaces/ISlide.ts
@@ -0,0 +1,8 @@
+export interface ISlide {
+    header: string;
+    text: string;
+    buttonText: string;
+    buttonLink?: string;
+    bgUrl: string;
+    textColor: "dark" | "light";
+}
